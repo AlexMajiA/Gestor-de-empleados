892b76376da5454e5c24b1ebc7acace4dba3f0b3
Alejandro Martínez <amj.parrales@gmail.com>
2025-03-10 12:07:03 +0100

    Primer commit, construcción del proyecto

---
+++ src/gestorempleados/Person.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestorempleados;
+
+/**
+ *
+ * @author amjpa
+ */
+public class Person {
+    
+    private String name;
+    private int salary;
+    private String department;
+
+    public Person() {
+    }
+
+    public Person(String name, int salary, String department) {
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    
+    
+    
+}
